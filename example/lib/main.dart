3a34a01075c2bcdc1c4239a975b15646626c4651
HoussemBousmaha <houssembousmaha.ksu@gmail.com>
2024-06-06 22:53:22 +0300

    fix example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:zego_express_engine_example/home/home_page.dart';
 
-// This is a simple publish & play stream demo,
-// shows the basic function of the SDK.
-
-// You can refer to this example topic demo for more use cases.
-// https://github.com/zegoim/zego-express-example-topics-flutter
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -14,10 +8,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ZegoExpressExample',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(), // Main page of this demo
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
     );
   }
 }
